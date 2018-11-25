e76c902339405f95c8cd295aa264665169578db0
Jihui yang <jihyang@localhost.localdomain>
2018-11-25 23:51:20 +0800

    change db config to adapt docker

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -17,9 +17,9 @@ const all = {
   // MongoDB connection options
   mongo: {
     options: {
-      db: {
-        safe: true
-      }
+      // db: {
+      //   safe: true
+      // }
     }
   },
 
