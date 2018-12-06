d52b0b5ad1ad772413cfdcb2becda66b103c6a99
Jihui yang <jihyang@dhcp-136-81.nay.redhat.com>
2018-12-06 17:37:30 +0800

    add slash

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -10,10 +10,10 @@ module.exports = {
 
   port: process.env.PORT || 9000,
 
-  // MongoDB connection options
+  //MongoDB connection options
   mongo: {
-    uri: 'mongodb://:@10.72.37.221:27017/cplant',
-   // uri: 'mongodb://localhost:27017/cplant-development',
+    //uri: 'mongodb://:@10.72.37.221:27017/cplant',
+    uri: 'mongodb://localhost:27017/cplant-development',
     options: {
       useNewUrlParser: true
     }
